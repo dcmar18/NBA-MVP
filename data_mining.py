4a0e9d31702ee25d3764926e98533872a253606e
dcmar18 <dcmar18@gmail.com>
2022-01-22 23:08:45 -0800

    Exported list of all players to a csv file and created a new py file for getting data via api. Jupyter notebook would time out

---
+++ data_mining.py
@@ -0,0 +1,34 @@
+#Import packages
+import pandas as pd
+import time
+from tqdm import tqdm
+from nba_api.stats.endpoints import commonallplayers
+from nba_api.stats.endpoints import commonplayerinfo
+from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.static import players
+
+all_players = pd.read_csv('./all_players.csv')
+all_players
+
+# Create list of all the player IDs
+players_ids = all_players['PERSON_ID']
+players_ids
+
+#Test getting stats for 1 player
+career = playercareerstats.PlayerCareerStats(player_id=76001)
+player_career = career.get_data_frames()[0]
+player_career
+
+# initial an empty dataframe
+players_stats = pd.DataFrame()
+# save fail queries
+error_log = []
+# extracting stats
+for ID in tqdm(players_ids):
+    try:
+        time.sleep(1) # avoid too many queries submitted at the same time
+        career = playercareerstats.PlayerCareerStats(player_id=ID)
+        player_career = career.get_data_frames()[0]
+        players_stats = pd.concat([players_stats,player_career],axis=0,ignore_index=True)
+    except:
+        error_log.append(ID)
